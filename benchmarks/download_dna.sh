d1fca5b4c65e77d2a63f494d332ae2e3a7599047
Matthias Niewerth <matthias.niewerth@tu-dortmund.de>
2020-09-11 16:48:07 +0200

    Add script to download chromosome-1.

---
+++ benchmarks/download_dna.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Download genome
+
+# download genome, unzip, extract first chromosome and change all letters to 
+# upper case
+
+# It is expected that the download aborts after the first chromosome is extracted.
+# There is no need to download the full genome.
+wget -O - ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.fna.gz 2>/dev/null | zcat | tr -d '\n' | tr acgt ACGT | tail -c +69 | head -c 248956422 > chromosome-1
+
